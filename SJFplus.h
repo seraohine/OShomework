51c0e4af81c2b3cf12cc8990df44317c86686294
seraophine <wx375645615@gmail.com>
2023-06-15 10:33:44 +0800

    the second commit

--- SJFplus.h
+++ SJFplus.h
@@ -30,15 +30,18 @@ int getNextTask(JOB job[], int quantity, int currentTime) {
 
 
 void SJFplus(JOB job[], int quantity) {
+    for (int i = 0; i < quantity; i++) {
+        job[i].visit = 0;
+    }
     int completed = 0;
     int currentTime = job[0].reachTime;
     int runIndex = -1;
     int reachIndex = -1;
     double roundTime = 0;
-
-    printf("-------------------------------------\n");
-    printf("作业编号   到达时间   开始时间   等待时间   作业长度   周转时间\n");
-    printf("-------------------------------------\n");
+    printf("可抢占式的最短作业优先调度:\n");
+    printf("--------------------------------------------------------\n");
+    printf("作业编号\t到达时间\t开始时间\t周转时间\n");
+    printf("--------------------------------------------------------\n");
 
     runIndex = getNextTask(job, quantity, currentTime);
     while (completed != quantity) {
@@ -49,18 +52,13 @@ void SJFplus(JOB job[], int quantity) {
         job[runIndex].reached = 1;
         reachIndex = getReachTask(job, quantity, currentTime);
 
-        // if (reachIndex != -1) {
-        //     job[runIndex].remainNeedTime = job[runIndex].remainNeedTime - (job[reachIndex].reachTime - job[runIndex].startTime);
-        //     currentTime += (job[reachIndex].reachTime - job[runIndex].startTime);
-        // }
-
-        for (int i = 0; i < quantity; i++) {
+        while(1) {
             if (reachIndex != -1) {
                 job[runIndex].remainNeedTime = job[runIndex].remainNeedTime - (job[reachIndex].reachTime - job[runIndex].startTime);
                 currentTime += (job[reachIndex].reachTime - job[runIndex].startTime);
             }
             if (job[runIndex].remainNeedTime > job[reachIndex].needTime && reachIndex != -1) {
-                printf("%-4d   %-4d   %-4d\n", job[runIndex].id, job[runIndex].reachTime, currentTime-job[runIndex].startTime);
+                printf("%-8d\t%-8d\t%-8d\t\t(被挂起)\n", job[runIndex].id, job[runIndex].reachTime, job[runIndex].startTime);
                 runIndex = reachIndex;
             } else {
                 reachIndex = getReachTask(job, quantity, currentTime);
@@ -72,27 +70,14 @@ void SJFplus(JOB job[], int quantity) {
                 completed++;
                 job[runIndex].runtime = currentTime - job[runIndex].reachTime;
                 roundTime += job[runIndex].runtime;
-                printf("%-4d   %-4d   %-4d   %-4d   %-4d   %-4d\n", job[runIndex].id, job[runIndex].reachTime, job[runIndex].startTime, job[runIndex].waitTime, job[runIndex].needTime, job[runIndex].runtime);
+                printf("%-8d\t%-8d\t%-8d\t%-8d\n", job[runIndex].id, job[runIndex].reachTime, job[runIndex].startTime, job[runIndex].runtime);
                 runIndex = getNextTask(job, quantity, currentTime);
                 break;
             }
             break;
         }
 
-        // job[runIndex].remainNeedTime = job[runIndex].remainNeedTime - (job[reachIndex].reachTime - job[runIndex].startTime);
-        // currentTime += (job[reachIndex].reachTime - job[runIndex].startTime);
-        // if (job[runIndex].remainNeedTime > job[reachIndex].needTime && reachIndex != -1) {
-        //     printf("%-4d   %-4d   %-4d\n", job[runIndex].id, job[runIndex].reachTime, currentTime-job[runIndex].startTime);
-        //     runIndex = getNextTask(job, quantity, currentTime);
-        // } else {
-        //     currentTime += job[runIndex].remainNeedTime;
-        //     job[runIndex].visit = 1;
-        //     completed++;
-        //     job[runIndex].runtime = currentTime - job[runIndex].reachTime;
-        //     roundTime += job[runIndex].runtime;
-        //     printf("%-4d   %-4d   %-4d   %-4d   %-4d   %-4d\n", job[runIndex].id, job[runIndex].reachTime, job[runIndex].startTime, job[runIndex].waitTime, job[runIndex].needTime, job[runIndex].runtime);
-        //     runIndex = getNextTask(job, quantity, currentTime);
-        // }
     }
-    printf("平均周转时间:%lf\n", roundTime/(double)quantity);
+    printf("--------------------------------------------------------\n");
+    printf("平均周转时间:%.2lf\n", roundTime/(double)quantity);
 }
--- job.h
+++ job.h
@@ -21,8 +21,7 @@ typedef struct JOB
     int runtime;        //作业周转时间
     int remainNeedTime; //剩余作业长度
     int reached;        //是否到达
-    struct JOB* next;
-}JOB, *task;
+}JOB;
 
 
 //作业初始化
@@ -56,42 +55,6 @@ void sort_reachTime(JOB *job) {
     }
 }
 
-void sort_needTime(JOB *job) {
-    int i,b=0,min,temp=0;
-    job[0].endtime = job[0].reachTime + job[0].needTime;//结束时间=到达时间+服务时间
-    for (i = 1; i < quantity; i++) {
-            if (job[i].reachTime > job[0].endtime);  //作业到达系统时，第0个作业还在运行
-            else b = b + 1;        //用b统计需等待作业0运行的作业个数
-    }
-
-    for (i = 1; i <= b - 1; i++) {
-        if (job[i].needTime > job[i + 1].needTime) {
-            min = job[i].reachTime;
-            job[i].reachTime = job[i + 1].reachTime;
-            job[i + 1].reachTime = min;
-
-            min = job[i].needTime;
-            job[i].needTime = job[i + 1].needTime;
-            job[i + 1].needTime = min;
-
-            temp = job[i].id;		//将第二个参数的值复制给第一个参数，返回第一个参数
-            job[i].id = job[i + 1].id;
-            job[i + 1].id = temp;
-        }                 //按最短运行时间排序
-    }
-    for (i = 1; i < quantity; i++) {
-        if (job[i].reachTime > job[i - 1].endtime) {     //第i个进程到达系统时，第i-1个进程已运行完毕
-            job[i].endtime = job[i].reachTime + job[i].needTime;
-            job[i].runtime = job[i].needTime;
-        }
-        else {
-            job[i].endtime = job[i - 1].endtime + job[i].needTime;
-            job[i].runtime = job[i].endtime - job[i].reachTime;
-        }
-    }
-}
-
-
 //读取作业信息并输出
 void readJOBdata(JOB *job) {
     FILE* file;
@@ -112,13 +75,14 @@ void readJOBdata(JOB *job) {
 
 //展示初始作业序列
 void showJobs(JOB job[]) {
+    printf("初始作业序列:\n");
     printf("-------------------------------------\n");
     printf("作业编号\t到达时间\t作业长度\n");
     printf("-------------------------------------\n");
     for (int i = 0; i < quantity; i++) {
-        printf("%4d\t%4d\t%4d\n", job[i].id, job[i].reachTime, job[i].needTime);
+        printf("%-8d\t%-8d\t%-8d\n", job[i].id, job[i].reachTime, job[i].needTime);
     }
-    printf("-------------------------------------\n");
+    printf("-------------------------------------\n\n");
 }
 
 #endif
