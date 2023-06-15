dd5497bdc3cdbfc554056bcf6af02bae05fd4746
seraophine <wx375645615@gmail.com>
2023-06-15 10:36:15 +0800

    the third commit

--- Main.c
+++ Main.c
@@ -3,8 +3,6 @@
 #include "HRRN.h"
 #include "SJFplus.h"
 #include "job.h"
-// #include "SJF4.h"
-// #include "SJF3.h"
 
 int main() {
     JOB job[MAXJOB];
@@ -12,10 +10,9 @@ int main() {
     readJOBdata(job);
     showJobs(job);
     sort_reachTime(job);
-     //FCFS(job, quantity);
-     //SJF(job, quantity);
-     //HRRN(job, quantity);
+    FCFS(job, quantity);
+    SJF(job, quantity);
+    HRRN(job, quantity);
     SJFplus(job, quantity);
-    // SJF3(job, quantity);
     return 0;
 }
